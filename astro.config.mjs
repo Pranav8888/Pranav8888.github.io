059af4f889dd34b96179e18fd448fdba151f77db
Pranav8888 <balamuralipranav@gmail.com>
2026-06-30 11:08:56 -0500

    Initial commit: Astro blog framework

--- astro.config.mjs
+++ astro.config.mjs
@@ -6,7 +6,7 @@ import { defineConfig, fontProviders } from 'astro/config';
 
 // https://astro.build/config
 export default defineConfig({
-	site: 'https://example.com',
+	site: 'https://Pranav8888.github.io',
 	integrations: [mdx(), sitemap()],
 	fonts: [
 		{
